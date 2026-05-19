d60d5ddea4ea60bc86872bcb46620f463a42dbc3
peppapig-03 <245528390+peppapig-03@users.noreply.github.com>
2026-05-19 09:59:27 +0800

    yml

---
+++ src/file1.js
@@ -0,0 +1,4 @@
+const cl123=function(){
+    console.log(123)
+}
+export default cl123
--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-import "./styles.css"
+import cl123 from "./file1.js"
+const cl456=function(){
+    console.log(456)
+}
+cl123()
+cl456()
