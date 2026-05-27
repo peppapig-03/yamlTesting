2e2da8cb75918ae7c6c5740d34e64307ec1f8e88
peppapig-03 <245528390+peppapig-03@users.noreply.github.com>
2026-05-27 17:06:27 +0800

    test repeat

--- src/index.js
+++ src/index.js
@@ -7,21 +7,15 @@ emailjs.init({
 })
 
 const sendEmail=async function(rainLocation, rainTime){
-    console.log(1)
     const details={
         location:rainLocation,
         time:rainTime
     }
-    console.log(2)
     try{
-        console.log(3)
         const response=await emailjs.send(config.serviceID,config.templateID,details)
-        console.log(4)
         console.log("Success!", response.status, response.text)
     } catch (error){
-        console.log(5)
         console.log("Failed...", error)
     }
 }
 sendEmail("Shah Alam", "1700")
-console.log(6)
